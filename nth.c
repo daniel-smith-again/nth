42a62ffb10cdf8513eb1aa695a14f28e6398964b
o_o <daniel.smith.again@gmail.com>
2023-02-20 09:52:44 -0500

    ...

--- nth.c
+++ nth.c
@@ -1,6 +1,36 @@
-#include "nth.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* linux specific includes */
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+unsigned long long int Expression_Count = 0;
+struct termios ui, restore;
+struct winsize window_size;
+
 
 int main ()
 {
+	tcgetattr(0, &restore);
+	ui = restore;
+	ui.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+	ui.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	ui.c_cflag &= ~(CSIZE | PARENB);
+	ui.c_cflag |= CS8;
+	tcsetattr(0, TCSANOW, &ui);
+	ioctl(0, TIOCGWINSZ, &window_size);
+
+	char c;
+
+	while(read(0, &c, 1) > 0)
+	{
+		if (c == '~') break; else write(1, &c, 1);
+	}
+	tcsetattr(0, TCSANOW, &restore);
 	return 0;
 }
