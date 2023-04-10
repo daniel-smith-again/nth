1241b6b301ccf31e0b8e941e3602a77d37fbca6b
o_o <daniel.smith.again@gmail.com>
2023-04-10 07:47:51 -0400

    ...

--- nth.c
+++ nth.c
@@ -9,14 +9,34 @@
 #include <termios.h>
 #include <unistd.h>
 
+
+
 unsigned long long int Expression_Count = 0;
-struct termios ui, restore;
 struct winsize window_size;
-typedef enum {Expression, Sequence, Collection, Type, Symbol, Quote, String, Number} NodeType;
 typedef unsigned long long int Register;
 typedef unsigned char Byte;
+
+struct termios ui, restore;
+void init_interface();
+void exit_interface();
+
+void *construct_program_model();
+void *compute_program_model();
+
+void display(void *d);
+void *read();
+
 int main ()
 {
+	char *c;
+	void *Program_Model;
+	init_interface();
+	
+EXIT: exit_interface();
+	return 0;
+}
+
+init_interface() {
 	tcgetattr(0, &restore);
 	ui = restore;
 	ui.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
@@ -25,19 +45,10 @@ int main ()
 	ui.c_cflag |= CS8;
 	tcsetattr(0, TCSANOW, &ui);
 	ioctl(0, TIOCGWINSZ, &window_size);
-	
-	char *c;
-	void *Program_Model;
-ReadExpression:
-ReadSequence:
-ReadCollection:
-ReadType:
-ReadSymbol:
-ReadQuote:
-ReadString:
-EXIT:
+}
+
+exit_interface() {
 	write(1, "\n", 1);
 	tcsetattr(0, TCSANOW, &restore);
 	exit(0);
-	return 0;
 }
