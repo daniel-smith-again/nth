680f85620ab93ea90c1da89ec9821d7d64c6a4eb
o_o <daniel.smith.again@gmail.com>
2024-02-16 14:40:08 -0500

    ...

--- src/nth.c
+++ src/nth.c
@@ -28,6 +28,7 @@ char *Buffer;
 Int BufferSize, BufferLength, Pos, Offset;
 char ReadChar();
 char PeekChar();
+void LineBreak();
 
 Program *Read();
 Program *Compute(Program *exp);
@@ -47,10 +48,14 @@ int main ()
   program = Read();
   if (program)
     //Print(Compute(program);
-    Print(program);
+    Print(program), LineBreak();
   else 
+  {
     Print(&Error);
-  if (program) Delete(program), program = 0;
+    LineBreak();
+    if (Buffer) free(Buffer), Buffer = 0, BufferSize = 0; BufferLength = 0; Offset = 0, Pos = 0;
+    if (program) Delete(program), program = 0;
+  }
   goto Begin;
   return 0;
 }
@@ -96,8 +101,14 @@ void StartShell()
   return;
 }
 
+void LineBreak()
+{
+  write(Out, "\r\n", 2);
+}
+
 void ExitShell()
 {
+  write(Out, "\r\n", 2);
   tcsetattr(0, TCSANOW, &restore);
   exit(1);
 }
@@ -109,85 +120,71 @@ char PeekChar()
   return c;
 }
 
+void Echo()
+{
+  char* str = 0;
+  write(Out, "\x1b[2K\r", 5);
+  if (Offset > window.ws_col)
+  {
+    str = 0;
+    str = malloc(sizeof(char) * (window.ws_col - 1));
+    if (str) strncpy(str, &Buffer[BufferLength - (window.ws_col - 1)], window.ws_col - 1);
+    else ExitShell();
+    write(Out, str, window.ws_col - 1);
+  }
+  else
+  {
+    str = 0;
+    str = malloc(sizeof(char) * (window.ws_col - 1));
+    if (str) strncpy(str, &Buffer[BufferLength - Offset], Offset);
+    else ExitShell();
+    write(Out, str, Offset);
+  }
+  free(str);
+
+}
+
 char ReadChar()
 {
-  char *str;
   char c[128];
-  Int r = 0;
+  int r = 0;
   if (BufferLength == 0 || Pos >= BufferLength)
   {
     BufferLength = 0;
     Pos = 0;
-    BufferSize = 0;
+    BufferSize = 1024;
     Buffer = realloc(Buffer, sizeof(char) * BufferSize);
     for (;;)
     {
+      r = 0;
       if (r = read(In, c, 128), r)
       {
         if (r > 1) continue;
         if (c[0] == 27) ExitShell();
-        if (c[0] == 8 || c[0] == 27)
+        else if (c[0] == 8 || c[0] == 127)
         {
           if (Offset > 0 && BufferLength > 0)
-          {
-            Offset--, BufferLength--;
-            while (Buffer[BufferLength - 1] > 127 && Offset > 0 && BufferLength > 0) Offset--, BufferLength--;
-            write(Out, "\x1b[2K\r", 5);
-            if (Offset > window.ws_col)
-            {
-              str = 0;
-              str = malloc(sizeof(char) * (window.ws_col - 1));
-              if (str) strncpy(str, &Buffer[BufferLength - (window.ws_col - 1)], window.ws_col - 1);
-              else ExitShell();
-              write(Out, str, window.ws_col - 1);
-            }
-            else
-            {
-              str = 0;
-              str = malloc(sizeof(char) * (window.ws_col - 1));
-              if (str) strncpy(str, &Buffer[BufferLength - Offset], Offset);
-              else ExitShell();
-              write(Out, str, Offset);
-            }
-            free(str);
-          }
-          continue;
+            BufferLength --, Offset--, Echo();
         }
-        if (c[0] > 9 && c[0] < 14)
+        else if (c[0] > 9 && c[0] < 14)
         {
+          write(Out, "\r\n", 2);
           BufferLength++;
           if (BufferLength >= BufferSize)
-            BufferSize += 1024, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
-          Buffer[BufferLength - 1] = '\n';
-          write(Out, "\n\r", 2);
+            BufferSize += 1028, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
+          Buffer[BufferLength - 1] = ' ';
           Offset = 0;
+          Echo();
           goto End;
         }
-        if (c[0] >= ' ')
+        else if (c[0] >= ' ')
         {
           BufferLength++;
           if (BufferLength >= BufferSize)
-            BufferSize += 1024, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
+            BufferSize += 1028, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
           Buffer[BufferLength - 1] = c[0];
           Offset++;
-          write(Out, "\x1b[2K\r", 5);
-          if (Offset > window.ws_col)
-          {
-            str = 0;
-            str = malloc(sizeof(char) * (window.ws_col - 1));
-            if (str) strncpy(str, &Buffer[BufferLength - (window.ws_col - 1)], window.ws_col - 1);
-            else ExitShell();
-            write(Out, str, window.ws_col - 1);
-          }
-          else
-          {
-            str = 0;
-            str = malloc(sizeof(char) * (window.ws_col - 1));
-            if (str) strncpy(str, &Buffer[BufferLength - Offset], Offset);
-            else ExitShell();
-            write(Out, str, Offset);
-          }
-          free(str);
+          Echo();
         }
       }
     }
@@ -197,83 +194,80 @@ char ReadChar()
   return c[0];
 }
 
+void Print(Program *data) 
+{
+  switch(data->type) {
+    case Symbol:
+      write(Out, data->symbol, data->size);
+      break;
+    case Expression:
+      write(Out, "(", 1);
+      for (Int n = 0; n < data->size; n++)
+      {
+        Print(data->expression[n]);
+        if (n < data->size - 1)
+        {
+          write(Out, " ", 1);
+        }
+      }
+      write(Out, ")", 1);
+      break;
+  }
+  return;
+}
+
 #endif
 
-Program *Read() 
+Program *Read()
 {
   Program *e = malloc(sizeof(Program)), *tmp = 0;
-  char c = PeekChar();
+  char c = 0;
   char *str = 0;
-  Int strl = 0;
-  Int isNum = 0;
-  Int Nmrl = 0, Dnml = 0;
-  switch(c) 
+  Int strl = 0, isNum = 0, Nmrl = 0, Dnml = 0;
+  TryAgain:
+  c = PeekChar();
+  switch(c)
   {
-    case ')': free(e); return 0;
+    case ' ': ReadChar(); goto TryAgain;
+    case ')': return 0;
     case '(':
       ReadChar();
       e->type = Expression;
-      e->expression = malloc(0);
       e->size = 0;
+      e->expression = malloc(0);
       for (c = PeekChar(); c != ')'; c = PeekChar())
-      {
-        if (c == ' ') continue;
-        tmp = Read();
-        if (tmp) {
-          e->size++;
-          e->expression = realloc(e->expression, sizeof(Program*) * e->size);
-          e->expression[e->size - 1] = tmp;
-        }
-        else return 0;
-      }
-      ReadChar();
-      return e;
-      break;
-    case '"':
-      ReadChar();
-      e->type = String;
-      strl = 0, str = malloc(0);
-      for (c = PeekChar(); c != '"'; c = PeekChar())
-      {
-        if (c == '\\') {
+        if (c == ' ') 
+        {
           ReadChar();
-          c = ReadChar();
+          continue;
         }
-        strl++, str = realloc(str, sizeof(char) * strl), str[strl - 1] = c, ReadChar();
-      }
-      e->size = strl;
-      e->string = str;
+        else 
+        {
+          tmp = Read();
+          if (tmp)
+          {
+            e->size++;
+            e->expression = realloc(e->expression, sizeof(Program*) * e->size);
+            e->expression[e->size - 1] = tmp;
+          }
+          else return 0;
+        }
+      ReadChar();
       return e;
-      break;
+    case '"':
     default:
-      strl = 0, str = malloc(0);
-      for (c = PeekChar(); c != ' ' || c != ')' || c != '"' || c != '('; c = PeekChar())
-        strl ++, str = realloc(str, sizeof(char) * strl), str[strl - 1] = c, ReadChar();
-      isNum = 1;
-      for (Int n = 0, radix = 0, places = 0; n < strl; n++)
-        if (str[n] >= '0' && str[n] <= '9') places++;
-        else if (!radix && places > 0 && n < strl - 1 && (str[n] == '.' || str[n] == '/')) radix = 1;
-        else {isNum = 0; break;}
-      e->size++;
-      e->expression = realloc(e->expression, sizeof(Program*) * e->size);
-      if (isNum) 
+      strl = 0, str = malloc(sizeof(char) * strl);
+      for (c = PeekChar(); c != ' ' && c != '(' && c != ')' && c != '"' ; c = PeekChar())
       {
-        e->type = Number;
-        Nmrl = 0; Dnml = 0;
-        for (Int n = 0; n < strl && str[n] >= '0' && str[n] <= '9'; n++) Nmrl++;
-        for (Int n = Nmrl + 1; n < strl && str[n] >= '0' && str[n] <='9'; n++) Dnml++;
-        e->numerator = malloc(sizeof(char) * Nmrl + 1);
-        for (Int n = 0; n < Nmrl; n++) e->numerator[n] = str[n];
-        e->denominator = malloc(sizeof(char) * Dnml + 1);
-        for (Int n = Nmrl + 1; n < strl; n++) e->denominator[n] = str[n];
-        e->size = 0;
+        strl++;
+        str = realloc(str, sizeof(char) * strl);
+        str[strl - 1] = c;
+        ReadChar();
       }
-      else 
-      {
-        e->type = Symbol;
-        e->size = strl;
-        e->symbol = str;
-      }
+      e->type = Symbol;
+      e->size = strl;
+      e->symbol = str;
+      return e;
   }
 }
 
@@ -298,22 +292,6 @@ void Delete(Program *n)
       }
       free(n->expression);
     break;
-  }
-  return;
-}
-
-void Print(Program *data) 
-{
-  switch(data->type) {
-    case Symbol:
-      write(Out, data->symbol, data->size);
-      break;
-    case Expression:
-      write(Out, "(", 1);
-      for (Int n = 0; n < data->size; n++)
-        Print(data->expression[n]);
-      write(Out, ")", 1);
-      break;
   }
   return;
 }
