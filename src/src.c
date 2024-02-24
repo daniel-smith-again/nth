cb467c23443799f3d0e8421e3e2c776a54c21fd0
Daniel Smith <daniel.smith.again@gmail.com>
2024-02-23 19:06:49 -0500

    ...

--- src/src.c
+++ src/src.c
@@ -1,70 +1,8 @@
 #define InfoString \
-"Welcome to the nth language utility.\r\n\
-Copyight (C) Daniel Smith daniel.smith.again@gmail.com"
-#include <stdio.h>
-#include <string.h>
+"Welcome to the nth language programming utility\r\n\
+Copyright (C) Daniel Smith daniel.smith.again@gmail.com"
 #include <stdlib.h>
-typedef unsigned long long int Int;
-typedef Int Word;
-typedef unsigned char Byte;
-
-typedef struct __program__ 
-{
-  Int size;
-  enum {Symbol, String, Number, Expression} type;
-  union {
-    struct __program__ **expression;
-    char *symbol;
-    char *string;
-    struct {char *numerator; Int nml; char*denominator; Int dnl;};
-  };
-} Program;
-
-Program Error = { .size = 1, .type = Symbol, .symbol = "?"};
-
-char *Buffer;
-Int BufferSize, BufferLength, Pos, Offset;
-char ReadChar();
-char PeekChar();
-void LineBreak();
-
-Program *Read();
-Program *Compute(Program *exp);
-void (*Compile())(Program *exp);
-void Print(Program *data);
-void Delete();
-
-void InitImage();
-void StartShell();
-void ExitShell();
-
-int main ()
-{
-  Program *program, *result;
-  StartShell();
-  InitImage();
-  Begin: program = Read();
-  if (program)
-  {
-    result = Compute(program);
-    if (result) Print(result);
-    else goto Error;
-    LineBreak();
-  }
-  else 
-  {
-    Error:
-    Print(&Error);
-    LineBreak();
-    if (Buffer) free(Buffer), Buffer = 0, BufferSize = 0; BufferLength = 0; Offset = 0, Pos = 0;
-    if (program) Delete(program), program = 0;
-  }
-  goto Begin;
-  return 0;
-}
-
-#ifdef Linux_AMD64
-
+#include <string.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -73,10 +11,205 @@ int main ()
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <dlfcn.h>
+#include <elf.h>
 #define In STDIN_FILENO
 #define Out STDOUT_FILENO
+typedef unsigned long long int Int;
+typedef enum {image, function, type, collection, string, range, vector, program, symbol, byte, word} Format;
+struct __image__;
+struct __function__;
+struct __type__;
+struct __collection__;
+struct __range__;
+struct __vector__; 
+struct __number__;
+struct __character__;
+struct __word__;
+struct __byte__;
+struct __place__;
+struct __string__;
+struct __program__;
+struct __unit__;
+
+typedef struct __character__
+{
+  unsigned long int c;
+} Character;
+
+typedef struct __word__
+{
+  Int w;
+} Word;
+
+typedef struct __byte__
+{
+  unsigned char b;
+} Byte;
+
+typedef struct __image__
+{
+  struct __unit__ *data;
+  struct __symbol__ *names;
+  Int length;
+  struct __image__ *parent;
+} Image;
+typedef struct __function__
+{
+  struct __byte__ IsCompiled;
+  void *text;
+  struct __program__ *definition;
+  struct __symbol__ *parameters;
+  struct __unit__ *types;
+  Int parametersl;
+  struct __unit__ *result;
+  struct __function__ *handlers;
+  Int handlersl;
+  struct __image__ *closure;
+} Function;
+
+typedef struct __type__
+{
+  enum{qualified, enumerated} kind;
+  union{
+    struct __function__ *pred;
+    struct __unit__ *members;
+  };
+  Int size;
+} Type;
+
+typedef struct __collection__
+{
+  struct __unit__ *fields;
+  struct __symbol__ *names;
+  Int size;
+} Collection;
+
+typedef struct __vector__
+{
+  struct __unit__ *kind;
+  struct __unit__ *indicies;
+  Int length;
+} Vector;
+
+typedef struct __number__
+{
+  char sign;
+  char *numerator;
+  char nl;
+  char *denominator;
+  char dl;
+} Number;
+
+typedef struct __range__
+{
+  struct __number__ min;
+  struct __number__ max;
+} Range;
+
+typedef struct __symbol__
+{
+  char *data;
+  Int length;
+} Symbol;
+
+typedef struct __place__
+{
+  void *p;
+} Place;
+
+typedef struct __string__
+{
+  struct __character__ *chars;
+  Int length;
+} String;
+
+typedef struct __program__ 
+{
+  enum {Sym, Exp, Str, Num} kind;
+  union {
+    struct __symbol__ sym;
+    struct {struct __program__ *e; Int length;};
+    struct __string__ str;
+    struct __number__ num;
+  };
+} Program;
+
+typedef struct __unit__
+{
+  Format which;
+  union {
+    struct __image__ i;
+    struct __function__ f;
+    struct __type__ t;
+    struct __collection__ c;
+    struct __range__ r;
+    struct __vector__ v;
+    struct __number__ n;
+    struct __character__ h;
+    //struct __word__ w;
+    Int w; //machine word
+    //struct __byte__ b;
+    unsigned char b; //machine byte
+    //struct __place__ a;
+    void * a; //place
+    struct __string__ s;
+    struct __symbol__ y;
+    struct __program__ p;
+
+  };
+} Unit;
+Unit ErrSym = {.which = (Format)symbol, .y = (Symbol){.data = "?", .length = 1}};
+
+void StartShell();
+void ExitShell();
+
+char *Buffer;
+Int BufferSize, BufferLength, Pos, Offset;
+char *Nesting;
+Int Nestingl;
+char ReadChar();
+char PeekChar();
+void LineBreak();
+
+Program *Read();
+void InitImage();
+Unit *Compute(Program *exp);
+void Print(Unit *exp);
+void Discard(Unit *u);
+void Delete(Program *p);
+void Quit();
+
+void main()
+{
+  StartShell();
+  InitImage();
+  Program *p;
+  Unit *r;
+  Nesting = malloc(sizeof(char) * 0);
+  Nestingl = 0;
+  Loop:
+  if (Nesting) free(Nesting);
+  Nesting = malloc(sizeof(char) * 0);
+  Nestingl = 0;
+  p = Read();
+  if (p) r = Compute(p);
+  else goto Error;
+  if (r) Print(r), LineBreak();
+  else goto Error;
+  goto Loop;
+  Error:
+  Print(&ErrSym), LineBreak();
+  if (p) Delete(p);
+  if (r) Discard(r);
+  p = 0, r = 0;
+  goto Loop;
+  return;
+}
+
 struct termios raw, restore;
 struct winsize window;
+
 void WinSizeCh(int signum)
 {
   ioctl(Out, TIOCGWINSZ, &window);
@@ -102,11 +235,6 @@ void StartShell()
 	write(Out, InfoString, sizeof(InfoString));
 	write(Out, "\r\n", 2);
   return;
-}
-
-void LineBreak()
-{
-  write(Out, "\r\n", 2);
 }
 
 void ExitShell()
@@ -170,7 +298,7 @@ char ReadChar()
           BufferLength ++;
           if (BufferLength >= BufferSize)
             BufferSize += 1028, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
-          Buffer[BufferLength - 1] = ' ';
+          Buffer[BufferLength - 1] = '\n';
           Offset = 0;
           Echo();
           goto End;
@@ -192,38 +320,11 @@ char ReadChar()
   return c[0];
 }
 
-void Print(Program *data) 
+void LineBreak()
 {
-  switch(data->type) {
-    case Symbol:
-      write(Out, data->symbol, data->size);
-      break;
-    case Expression:
-      write(Out, "(", 1);
-      for (Int n = 0; n < data->size; n++)
-      {
-        Print(data->expression[n]);
-        if (n < data->size - 1)
-        {
-          write(Out, " ", 1);
-        }
-      }
-      write(Out, ")", 1);
-      break;
-    case Number:
-      write(Out, data->numerator, data->nml);
-      if (data->dnl)
-        write(Out, "/", 1), write(Out, data->denominator, data->dnl);
-      break;
-    case String:
-      write(Out, "\"", 1);
-      write(Out, data->string, data->size);
-      write(Out, "\"", 1);
-      break;
-  }
-  return;
+  write(Out, "\r\n", 2);
 }
-#endif
+
 
 Program *Read()
 {
@@ -236,16 +337,31 @@ Program *Read()
   switch(c)
   {
     case ' ': ReadChar(); goto TryAgain;
-    case ')': return 0;
+    case '\n': 
+      ReadChar();
+      for (Int n = Nestingl; n > 0; n--)
+        write(Out, &Nesting[n - 1], 1);
+      goto TryAgain;
+    case ')': ReadChar(); return 0;
     case '(':
       ReadChar();
-      e->type = Expression;
-      e->size = 0;
-      e->expression = malloc(0);
+      Nestingl++;
+      Nesting = realloc(Nesting, sizeof(char) * Nestingl);
+      Nesting[Nestingl - 1] = ')';
+      e->kind = Exp;
+      e->length = 0;
+      e->e = malloc(sizeof(Program) * e->length);
       for (c = PeekChar(); c != ')'; c = PeekChar())
-        if (c == ' ') 
+        if (c == ' ')
+        {
+          ReadChar(); 
+          continue;
+        }
+        else if (c == '\n')
         {
           ReadChar();
+          for (Int n = Nestingl; n > 0; n--)
+            write(Out, &Nesting[n - 1], 1);
           continue;
         }
         else 
@@ -253,260 +369,226 @@ Program *Read()
           tmp = Read();
           if (tmp)
           {
-            e->size++;
-            e->expression = realloc(e->expression, sizeof(Program*) * e->size);
-            e->expression[e->size - 1] = tmp;
+            e->length++;
+            e->e = realloc(e->e, sizeof(Program) * e->length);
+            e->e[e->length - 1] = *tmp;
           }
           else return 0;
         }
       ReadChar();
+      if (Nestingl > 0) Nestingl--;
       return e;
     case '"':
       ReadChar();
-      strl = 0, str = malloc(sizeof(char) * strl);
+      Nestingl++;
+      Nesting = realloc(Nesting, sizeof(char) * Nestingl);
+      Nesting[Nestingl - 1] = '"';
+      e->kind = Exp;
+      e->length = 1;
+      e->e = malloc(sizeof(Program) * e->length);
+      e->e[0].kind = Sym;
+      e->e[0].sym.data = malloc(sizeof(char) * 0);
+      e->e[0].sym.length = 0;
       for (c = PeekChar(); c != '"'; c = PeekChar())
       {
+        if (c == '\n') c = ' ';
         if (c == '\\')
-          ReadChar(), c = PeekChar();
-        strl++, str = realloc(str, sizeof(char) * strl), str[strl - 1] = c, ReadChar();
+        {
+          ReadChar();
+          c = PeekChar();
+          if (c == '\n') c = ' ';
+          if (c == '(')
+          {
+            e->length++;
+            e->e = realloc(e->e, sizeof(Program) * e->length);
+            tmp = Read();
+            if (tmp) e->e[e->length - 1] = *tmp, free(tmp);
+            else return 0;
+            e->length++;
+            e->e = realloc(e->e, sizeof(Program) * e->length);
+            e->e[e->length - 1].kind = Sym;
+          }
+          else {
+            e->e[e->length - 1].length++;
+            e->e[e->length - 1].sym.data = realloc(e->e[e->length - 1].sym.data, sizeof(char) * e->e[e->length - 1].sym.length);
+            e->e[e->length - 1].sym.data[e->e[e->length - 1].sym.length - 1] = c;
+            ReadChar();
+          }
+        }
+        else {
+            e->e[e->length - 1].length++;
+            e->e[e->length - 1].sym.data = realloc(e->e[e->length - 1].sym.data, sizeof(char) * e->e[e->length - 1].sym.length);
+            e->e[e->length - 1].sym.data[e->e[e->length - 1].sym.length - 1] = c;
+            ReadChar();
+        }
       }
-      e->type = String, e->size = strl, e->string = str, ReadChar();
-      return e;
+      if (Nestingl > 0) Nestingl--;
+      ReadChar();
+      if (e->length < 2)
+      {
+        str = e->e[e->length - 1].sym.data;
+        strl = e->e[e->length - 1].sym.length;
+        free(e->e);
+        e->kind = Str;
+        e->str.length = strl;
+        e->str.chars = (Character*)str;
+        return e;
+      }
+      else {
+        e->kind = Exp;
+        for (Int n = 0; n < e->length; n++)
+          if (e->e[n].kind == Sym)
+          {
+            str = e->e[n].sym.data;
+            strl = e->e[n].sym.length;
+            e->e[n].kind = Str;
+            e->e[n].str.chars = (Character*)str;
+            e->e[n].str.length = strl;
+          }
+        e->length++;
+        e->e = realloc(e->e, sizeof(Program) * e->length);
+        for (Int n = e->length - 1; n > 0; n--)
+          e->e[n] = e->e[n - 1];
+        e->e[0].kind = Sym;
+        e->e[0].sym.data = malloc(sizeof(char));
+        e->e[0].sym.length = 1;
+        e->e[0].sym.data[0] = '+';
+        return e;
+      }
+      break;
     default:
       strl = 0, str = malloc(sizeof(char) * strl);
-      for (c = PeekChar(); c != ' ' && c != '(' && c != ')' && c != '"' ; c = PeekChar())
+      for (c = PeekChar(); c != ' ' && c != '(' && c != ')' && c != '"' && c != '\n'; c = PeekChar())
         strl++, str = realloc(str, sizeof(char) * strl), str[strl - 1] = c, ReadChar();
-      e->type = Number;
-      e->nml = 0;
-      e->dnl = 0;
+      e->kind = Num;
+      e->num.nl = 0;
+      e->num.dl = 0;
       for (Int n = 0, radix = 0; n < strl; n++)
         if (str[n] >= '0' && str[n] <= '9')
-          if (radix) e->dnl++;
-          else e->nml++;
-        else if (n < strl - 1 && !radix && (str[n] == '.' || str[n] == '/')) radix = 1;
-        else e->type = Symbol;
-      if (e->type == Number)
+          if (radix) e->num.dl++;
+          else e->num.nl++;
+        else if (n < strl - 1 && !radix && str[n] == '.') radix = 1;
+        else if (n < strl - 1 && !radix && str[n] == '/') radix = 2;
+        else if (n == 0 && str[n] == '-') e->num.sign = 1;
+        else if (n == 0 && str[n] == '+') e->num.sign = 0;
+        else e->kind = Sym;
+      if (e->kind == Num)
       {
-        if (e->nml < strl && str[e->nml] == '.')
+        if (e->num.sign) e->num.sign = 1;
+        else e->num.sign = 0;
+        if (e->num.nl < strl && str[e->num.nl] == '.')
         {
-          e->dnl ++;
-          e->denominator = malloc(sizeof(char) * e->dnl);
-          for (Int n = 0; n < e->dnl; n++)
-            if (n == 0) e->denominator[n] = '1';
-            else e->denominator[n] = '0';
-          e->nml = strl - 1;
-          e->numerator = malloc(sizeof(char) * strl - 1);
+          e->num.dl ++;
+          e->num.denominator = malloc(sizeof(char) * e->num.dl);
+          for (Int n = 0; n < e->num.dl; n++)
+            if (n == 0) e->num.denominator[n] = '1';
+            else e->num.denominator[n] = '0';
+          e->num.nl = strl - 1;
+          e->num.numerator = malloc(sizeof(char) * strl - 1);
           for (Int n = 0, m = 0; n < strl; n++)
             if (str[n] == '.') continue;
-            else e->numerator[m] = str[n], m++;
+            else e->num.numerator[m] = str[n], m++;
         }
-        else if (e->nml < strl && str[e->nml] == '/')
+        else if (e->num.nl < strl && str[e->num.nl] == '/')
         {
-          e->numerator = malloc(sizeof(char) * e->nml);
-          e->denominator = malloc(sizeof(char) * e->dnl);
-          strncpy(e->numerator, str, e->nml);
-          strncpy(e->denominator, &str[e->nml + 1], e->dnl);
+          e->num.numerator = malloc(sizeof(char) * e->num.nl);
+          e->num.denominator = malloc(sizeof(char) * e->num.dl);
+          strncpy(e->num.numerator, str, e->num.nl);
+          strncpy(e->num.denominator, &str[e->num.nl + 1], e->num.dl);
         }
         else
         {
-          e->dnl = 0;
-          e->numerator = malloc(sizeof(char) * e->nml);
-          strncpy(e->numerator, str, e->nml);
+          e->num.dl = 0;
+          e->num.numerator = malloc(sizeof(char) * e->num.nl);
+          strncpy(e->num.numerator, str, e->num.nl);
         }
-        e->type = Number;
-        e->size = 0;
+        strl = 0;
+        for (Int n = 0; n < e->num.nl; n++)
+          if (e->num.numerator[n] == '0') strl++;
+          else break;
+        for (Int n = 0; n < e->num.nl; n++)
+          e->num.numerator[n] = e->num.numerator[n + strl];
+        e->num.nl -= strl, e->num.numerator = realloc(e->num.numerator, sizeof(char) * e->num.nl);
         free(str);
         return e;
       }
       else
       {
-        e->type = Symbol;
-        e->size = strl;
-        e->symbol = str;
+        e->kind = Sym;
+        e->sym.length = strl;
+        e->sym.data = str;
       }
       return e;
   }
 }
-
-void Delete(Program *n)
+void AddNumber()
 {
-  switch(n->type)
-  {
-    case Symbol:
-      free(n->symbol);
-    break;
-    case String:
-      free(n->string);
-      break;
-    case Number:
-      free(n->numerator), free(n->denominator);
-      break;
-    case Expression:
-      for (Int i = 0; i < n->size; i++)
-      {
-        Delete(n->expression[i]);
-        free(n->expression[i]);
-      }
-      free(n->expression);
-    break;
-  }
-  return;
+
 }
-
-typedef enum {Image, Function, Type, Collection, Range, Vector, ProgramModel, SymbolConstant} Format;
-struct __image__;
-struct __function__;
-struct __type__;
-struct __collection__;
-struct __range__;
-struct __vector__;
-struct __number__;
-struct __word__;
-struct __byte__;
-struct __place__;
-struct __symbol__;
-
-typedef struct __image__
-{
-  struct __symbol__* names;
-  struct __unit__* data;
-  Int size;
-} image;
-
-typedef struct __function__ 
-{
-  Byte IsCompiled;
-  char *definition;
-  Int definitionl;
-  Program *body;
-  void (*compiledBody)();
-  struct __symbol__ *parameters;
-  Int parametersl;
-  struct __unit__ *types;
-  struct __unit__ *result;
-  struct __function__ *handlers;
-  Int handlersl;
-  struct __image__ *closure;
-} function;
-
-typedef struct __type__
-{
-  enum {qualified, enumerated} which;
-  union {
-    struct __function__ *pred;
-    struct __unit__ **members;
-  };
-  Int membersl;
-} type;
-
-typedef struct __collection__
-{
-  struct __unit__ *fields;
-  char **names;
-  Int fieldsl;
-} collection;
-
-typedef struct __range__
-{
-  struct __number__ *min;
-  struct __number__ *max;
-} range;
-
-typedef struct __vector__
-{
-  struct __unit__ *kind;
-  struct __unit *indicies;
-  Int length;
-} vector;
-
-typedef struct __number__
-{
-  enum {Natural, Integer, Rational} subset;
-  Byte sign;
-  Byte *numerator;
-  Int numeratorl;
-  Byte *denominator;
-  Int denominatorl;
-} number;
-
-typedef struct __word__ 
-{
-  unsigned long long int w;
-} word;
-
-typedef struct __byte__
-{
-  unsigned char b;
-} byte;
-
-typedef struct __symbol__
-{
-  Int length;
-  char* data;
-} symbol;
-
-typedef struct __place__
-{
-  void *p;
-} place;
-
-typedef struct __unit__ 
-{
-  Format kind;
-  union {
-    struct __image__ i;
-    struct __function__ f;
-    struct __type__ t;
-    struct __collection__ c;
-    struct __range__ r;
-    struct __vector__ v;
-    struct __number__ n;
-    struct __word__ w;
-    struct __byte__ b;
-    struct __place__ p;
-    struct __symbol__ s;
-  };
-} Unit;
-
-#ifdef Linux_AMD64
-
-image *Env;
-Int EnvLevel;
-image *TopLevel;
-
+Image *environment;
+Image *toplevel;
 void InitImage()
 {
-  Env = malloc(sizeof(image));
-  TopLevel = Env;
-  EnvLevel = 0;
-  TopLevel->size = 0;
 }
 
-number AddNummbers(number *x, number *y)
+Unit *Compute(Program *exp)
 {
-  number *r = malloc(sizeof(number));
-  r->numeratorl = 0;
-  r->denominatorl = 0;
+  Unit *u = malloc(sizeof(Unit));
+  u->which = program;
+  u->p = *exp;
+  return u;
 }
 
-Program *Compute(Program *exp)
+void PrintProgram(Program p)
 {
-  Unit *Stack;
-  Int StackSize;
-  Int Frame;
-  Program *E;
-  E = exp;
-  for (Int n = 0; n < TopLevel->size; n++)
+  switch(p.kind)
   {
-
+    case Sym:
+      write(Out, p.sym.data, p.sym.length);
+      break;
+    case Exp:
+      write(Out, "(", 1);
+      for (Int n = 0; n < p.length; n++)
+        PrintProgram(p.e[n]), write(Out, " ", (n < p.length - 1 ? 1 : 0));
+      write(Out, ")", 1);
+      break;
+    case Str:
+      write(Out, "\"", 1);
+      write(Out, (char*)p.str.chars, p.str.length);
+      write(Out, "\"", 1);
+      break;
+    case Num:
+      write(Out, p.num.numerator, p.num.nl);
+      if (p.num.dl) write(Out, "/", 1), write(Out, p.num.denominator, p.num.dl);
   }
-  
 }
 
-void (*Compile())(Program *exp)
+void Print(Unit *exp)
 {
-  
-  return 0;
+  switch(exp->which)
+  {
+    case symbol:
+      write(Out, exp->y.data, exp->y.length);
+      break;
+    case collection:
+      break;
+    case program:
+      PrintProgram(exp->p);
+      break;
+  }
 }
 
-#endif
+void Discard(Unit *u)
+{
+
+}
+
+void Delete(Program *p)
+{
+
+}
+
+void Quit()
+{
+
+}
