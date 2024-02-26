33a26c5ff8dfb6d90ff6bd6e974a4ef54f65c443
o_o <daniel.smith.again@gmail.com>
2024-02-26 10:49:42 -0500

    ...

--- src/nth.c
+++ src/nth.c
@@ -8,9 +8,31 @@ typedef struct {
   Int type;
 } Data;
 
-void Read();
-void Compute();
-void Print();
+void* Read();
+void Compute(void *program);
+void Print(void *data);
+
+void StartShell();
+void LineBreak();
+void ExitShell();
+
+void main()
+{
+  StartShell();
+  void *p, *r;
+  Loop:
+    p = Read();
+    if (p) r = Compute(p);
+    else goto Err;
+    if (r) Print(r);
+    else goto Err;
+    goto Loop;
+  Err:
+    if (p) free(p);
+    if (r) free(r);
+    goto Loop;
+  ExitShell();
+}
 
 #ifdef Linux_AMD64
 #include <fcntl.h>
@@ -26,3 +48,141 @@ void Print();
 #define In STDIN_FILENO
 #define Out STDOUT_FILENO
 typedef unsigned long long int Int;
+struct termios raw, restore;
+struct winsize window;
+void WinSizeCh(int signum)
+{
+  ioctl(Out, TIOCGWINSZ, &window);
+}
+
+void LineBreak()
+{
+  write(Out, "\n\r", 2);
+}
+
+void StartShell()
+{
+  atexit(ExitShell);
+  tcgetattr(0, &restore);
+  raw = restore;
+  raw.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_cflag &= ~(CSIZE | PARENB);
+	raw.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+	tcsetattr(0, TCSANOW, &raw);
+	ioctl(Out, TIOCGWINSZ, &window);
+	signal(SIGWINCH, WinSizeCh);
+	//write(Out, "\x1b[2J\x1b[H", 7);
+	write(Out, "\r\n", 2);
+	write(Out, InfoString, sizeof(InfoString));
+	write(Out, "\r\n", 2);
+  return;
+}
+
+void ExitShell()
+{
+  write(Out, "\r\n", 2);
+  tcsetattr(0, TCSANOW, &restore);
+  exit(1);
+}
+
+void Read()
+{
+  char *Buffer;
+  Int BufferSize;
+  Int BufferLength;
+  Int Pos;
+  Int Offset;
+  void *program;
+  void Echo()
+  {
+    char* str = 0;
+    write(Out, "\x1b[2K\r", 5);
+    if (Offset > window.ws_col)
+    {
+      str = 0, str = malloc(sizeof(char) * (window.ws_col - 1));
+      if (str) strncpy(str, &Buffer[BufferLength - (window.ws_col - 1)], window.ws_col - 1);
+      else ExitShell();
+      write(Out, str, window.ws_col - 1);
+    }
+    else
+    {
+      str = 0, str = malloc(sizeof(char) * (window.ws_col - 1));
+      if (str) strncpy(str, &Buffer[BufferLength - Offset], Offset);
+      else ExitShell();
+      write(Out, str, Offset);
+    }
+    free(str);
+  }
+  char ReadChar()
+  {
+    char c[128];
+    int r = 0;
+    if (BufferLength == 0 || Pos >= BufferLength)
+    {
+      BufferLength = 0, Pos = 0, BufferSize = 1024, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
+      for (;;)
+      {
+        r = 0;
+        if (r = read(In, c, 128), r)
+        {
+          if (r > 1) continue;
+          if (c[0] == 27) ExitShell();
+          else if (c[0] == 8 || c[0] == 127)
+          {
+            if (Offset > 0 && BufferLength > 0)
+              BufferLength --, Offset--, Echo();
+          }
+          else if (c[0] > 9 && c[0] < 14)
+          {
+            write(Out, "\r\n", 2);
+            BufferLength ++;
+            if (BufferLength >= BufferSize)
+              BufferSize += 1028, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
+            Buffer[BufferLength - 1] = '\n';
+            Offset = 0;
+            Echo();
+            goto End;
+          }
+          else if (c[0] >= ' ')
+          {
+            BufferLength++;
+            if (BufferLength >= BufferSize)
+              BufferSize += 1028, Buffer = realloc(Buffer, sizeof(char) * BufferSize);
+            Buffer[BufferLength - 1] = c[0];
+            Offset++;
+            Echo();
+          }
+        }
+      }
+    }
+    End:
+    c[0] = Buffer[Pos], Pos++;
+    return c[0];
+  }
+  char PeekChar()
+  {
+    char c = ReadChar();
+    Pos--;
+    return c;
+  }
+  void *Construct()
+  {
+
+  }
+}
+
+void Compute()
+{
+
+}
+
+void Print()
+{
+
+}
+
+#endif
