f64748cf67172177aab8123784324ca2f7c85c9f
Daniel Smith <daniel.smith.again@gmail.com>
2024-05-26 13:14:56 -0400

    ...

---
+++ src/nth.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *Env;
+void *Image;
+
+void *Read();
+void *Compute();
+void *Display();
+
+int main() {
+
+}
+
+void *Read() 
+{
+
+}
+
+void *Compute()
+{
+
+}
+
+void *Display()
+{
+  
+}
