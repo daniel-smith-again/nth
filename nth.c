65857a3f8d2f2e05eb38e63251f3620ff417adad
o_o <daniel.smith.again@gmail.com>
2024-07-09 18:06:40 -0400

    ...

--- nth.c
+++ nth.c
@@ -69,7 +69,8 @@ typedef enum
         { 
           symbol, 
           number, 
-          string, 
+          string,
+          stringportion,
           expression, 
           quote, 
           unquote, 
@@ -79,48 +80,189 @@ typedef struct nth__syntax
 {
   Kind kind;
   Int size;
-  union { Int content; struct nth__syntax *nodes; };
+  union { Int content; struct nth__syntax **nodes; };
 } Syntax;
+Int InsideQuote;
 
-struct nth__input
+struct
 {
   char *content;
   Int size;
   Int i;
-  Syntax *nodes;
-  Int length;
-};
-struct nth__input Input;
+  int e;
+  Syntax *top;
+  Syntax **nodes;
+  Int count;
+} input;
 
 char Char();
 char Peek();
 Syntax *CreateNode(Kind k);
 void AppendNode(Syntax *a, Syntax *b);
 void DropBuffer();
-void Parse();
+Syntax *Parse();
+void Print(Syntax *s);
 
 int main ()
 {
-  Input.content = malloc(0);
-  Input.size = 0;
-  Input.i = 0;
-  Input.nodes = malloc(0);
-  Int length = 0;
+  DropBuffer();
+  for(;;)
+  {
+    input.top = Parse();
+    if (input.top)
+    {
+      Print(input.top);
+    }
+    else
+    {
+      printf("Parse Error\n");
+      for (Int i = 0; i < input.e; i++)
+        putchar(input.content[i]);
+    }
+    DropBuffer();
+    putchar('\n');
+  }
   return 0;
 }
 
 char Char()
 {
-  if (Input.i >= Input.size)
+  if (input.i >= input.size)
   {
-    for ( char c = 0; 1;
+    for ( char c = 0; 
+          1;
           c = getchar(),
-          Input.size++,
-          Input.content = realloc(Input.content, sizeof(char) * Input.size),
-          Input.content[Input.size-1] = c)
+          input.size++,
+          input.content = realloc(input.content, sizeof(char) * input.size),
+          input.content[input.size-1] = c)
       {if (c == '\n') break;}
   }
-  char c = Input.content[Input.i];
-  Input.i++;
+  char c = input.content[input.i];
+  input.i++;
   return c;
+}
+
+char Peek()
+{
+  char c = Char();
+  input.i--;
+  return c;
+}
+
+Syntax *CreateNode(Kind k)
+{
+  Syntax *n = malloc(sizeof(Syntax));
+  n->kind = k;
+  n->size = 0;
+  n->content = 0;
+  n->nodes = 0;
+  input.count++;
+  input.nodes = realloc(input.nodes, sizeof(Syntax*) * input.count);
+  input.nodes[input.count-1] = n;
+  return n;
+}
+
+void AppendNode(Syntax *a, Syntax *b)
+{
+  switch(a->kind)
+  {
+    case quote:
+    case unquote:
+    case requote:
+      if (a->size == 1) break;
+    case expression:
+      a->size++;
+      a->nodes = realloc(a->nodes, sizeof(Syntax*) * a->size);
+      a->nodes[a->size-1] = b;
+  }
+}
+
+void DropBuffer()
+{
+  if (input.nodes) for (Int i = 0; i < input.count; i++) 
+                        free(input.nodes[i]->nodes),
+                        free(input.nodes[i]);
+  if (input.content) free(input.content);
+  input.nodes = malloc(0);
+  input.content = malloc(0);
+  input.count = 0;
+  input.size = 0;
+  input.i = 0;
+  input.e = -1;
+  InsideQuote = 0;
+}
+
+Syntax *Parse()
+{
+  Int alreadyquoted = 0;
+  Syntax *t, *S;
+  backtrack:
+  switch(Peek())
+  {
+    case '(':
+      Char();
+      for (S = CreateNode(expression); Peek() != ')';)
+      {
+        t = Parse();
+        if (!t) goto ParseError;
+        else AppendNode(S, t);
+      }
+      Char();
+      return S;
+      break;
+    case ' ':
+    case '\n':
+      Char();
+      while (Peek() <= ' ') Char();
+      goto backtrack;
+      break;
+    case ')': goto ParseError;
+    case '\'':
+      Char();
+      S = CreateNode(quote);
+      if (Peek() == '\'') { Char(); S->kind = unquote; }
+      if (Peek() == '\'') { Char(); S->kind = requote; }
+      if (S->kind != quote && !InsideQuote) goto ParseError;
+      if (InsideQuote) alreadyquoted = 1;
+      else InsideQuote = 1;
+      S->size = 1;
+      S->nodes = malloc(sizeof(Syntax*));
+      t = Parse();
+      if (t == 0) goto ParseError;
+      else S->nodes[0] = t;
+      if (!alreadyquoted) InsideQuote = 0;
+      return S;
+    default:
+      for (S = CreateNode(symbol), S->content = input.i, S->size = 1, Char();; S->size++, Char())
+      { if (Peek() <= ' ') { Char(); return S; }
+        else if (Peek() == '(' || Peek() == ')') return S;
+      }
+      break;
+  }
+  ParseError:
+  if (input.e == -1) input.e = input.i;
+  free(S);
+  return 0;
+}
+
+void Print(Syntax *s)
+{
+  switch(s->kind)
+  {
+    case expression:
+      putchar('(');
+      for (Int i = 0; i < s->size; i++) 
+        Print(s->nodes[i]), i < s->size - 1 ? putchar(' ') : 0;
+      putchar(')');
+      break;
+    case quote: printf("'"); goto PrintQuote; break;
+    case unquote: printf("''"); goto PrintQuote; break;
+    case requote: printf("'''"); goto PrintQuote; break;
+      PrintQuote: Print(s->nodes[0]);
+      break;
+    case symbol:
+      for (Int i = 0; i < s->size; i++)
+        putchar(input.content[s->content+i]);
+      break;
+  }
 }
