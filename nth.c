17a4b4e11c11e2097849554fc022fc3cbb9f0803
Daniel Smith <daniel.smith.again@gmail.com>
2025-02-21 16:01:23 -0500

    ...

--- nth.c
+++ nth.c
@@ -11,52 +11,51 @@
 --------------------------------------------------------------------------------
 
                   This is a fully conforming implementation of                  
-                  the Nth Programming Language as described in                  
-                    "The nth Programming Language HyperDoc".                    
+                         the Nth Programming Language.                          
 
 
 --------------------------------------------------------------------------------
                                   Terms of Use                                 
 --------------------------------------------------------------------------------
 
-                  The author(s) grants the holder of this work                 
-             full right of access, without limitation, irrevocably,            
-                 in perpetuity, for any purpose, by any means.                 
+                  The author(s) grants the holder of this work                  
+             full right of access, without limitation, irrevocably,             
+                 in perpetuity, for any purpose, by any means.                  
 
-           Authorship of this work is reserved only for contributions          
-       to this work which are made under the terms given in this section.      
-             Contribution to this work under terms other than those            
-                given in this section constitutes authorship of                
-                    a distinct and separate work, for which                    
-                the status of authorship of this particular work               
-                                is not granted.                                
+           Authorship of this work is reserved only for contributions           
+       to this work which are made under the terms given in this section.       
+             Contribution to this work under terms other than those             
+                given in this section constitutes authorship of                 
+                    a distinct and separate work, for which                     
+                the status of authorship of this particular work                
+                                is not granted.                                 
 
-            In plain terms, if you, the rights holder of this work,            
-           create any derivation, modification, or copy of this work,          
-           you cannot claim that it is a form, version, or variation           
-              of the work titled "The nth Programming Environment"             
-         without providing it under the terms set out in this section.         
+            In plain terms, if you, the rights holder of this work,             
+           create any derivation, modification, or copy of this work,           
+           you cannot claim that it is a form, version, or variation            
+              of the work titled "The nth Programming Environment"              
+         without providing it under the terms set out in this section.          
 
-               The purpose of these terms is to provide the most               
-               permissive rights and allowances while preventing               
-                any individual or party from claiming authorship               
-                          with less permissive rights                          
-                and thus tainting the availability of the work.                
+               The purpose of these terms is to provide the most                
+               permissive rights and allowances while preventing                
+                any individual or party from claiming authorship                
+                          with less permissive rights                           
+                and thus tainting the availability of the work.                 
 
 
 --------------------------------------------------------------------------------
                              STATEMENT OF WARRANTY                             
 --------------------------------------------------------------------------------
 
-        THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       
-                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO               
-      THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,     
-                              AND NONINFRINGEMENT.                             
+        THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        
+                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO                
+      THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,      
+                              AND NONINFRINGEMENT.                              
 
-        IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES,        
-   OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE,   
-     ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE, OR THE USE,     
-                       OR OTHER DEALINGS IN THE SOFTWARE.                      
+        IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES,         
+   OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE,    
+     ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE, OR THE USE,      
+                       OR OTHER DEALINGS IN THE SOFTWARE.                       
 
 
 *******************************************************************************/
