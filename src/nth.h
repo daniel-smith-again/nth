128e63ea2988712cdb555b32930c2744cea4abb5
o_o <daniel.smith.again@gmail.com>
2025-03-03 14:37:58 -0500

    ...

---
+++ src/nth.h
@@ -0,0 +1,12 @@
+#ifndef _nth_header_lib_
+#define _nth_header_lib_
+
+typedef struct { unsigned char value; } nth_boolean;
+typedef struct { unsigned int value; } nth_character;
+//should cover every real that's not a transcendental number.
+typedef struct {  } nth_number;
+typedef struct {} nth_array;
+typedef struct {} nth_list;
+typedef struct {} nth_data;
+
+#endif
