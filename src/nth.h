05b491407817719598c2faf096fa7f1d0b4cf1e9
o_o <daniel.smith.again@gmail.com>
2025-03-04 13:31:39 -0500

    ...

--- src/nth.h
+++ src/nth.h
@@ -12,6 +12,49 @@ typedef struct { Int length; void** data; } nth_tuple;
 typedef struct { Int length; void** data; } nth_type;
 typedef struct { Int size; void* data; } nth_data;
 
-
+nth_array *nthUTF8toUTF32(nth_array *str)
+  Int l = 0;
+  for (Int i = 0; i < str->length; i++)
+  {
+    switch((Byte)str.data[i] & 0b11110000)
+    {
+      case 0b11000000 : l++; i++; break;
+      case 0b11100000 : l++; i += 2; break;
+      case 0b11110000 : l++; i += 3; break;
+      default: l++;
+    }
+  }
+  nth_array *outstr = malloc(sizeof(nth_array) + (sizeof(Int) * l));
+  outstr->length = l;
+  outstr->data = outstr + (sizeof(nth_array));
+  for (Int i = 0, j = 0; i < outstr->length; i++, j++)
+  {
+    outstr->data[i] = 0;
+    switch((Byte)str.data[i] & 0b11110000)
+    {
+      case 0b11000000 : 
+        outstr->data[j] |= (Int)str.data[i] << 3;
+        outstr->data[j] |= (Int)str.data[i+1] << 2;
+        i++;
+        break;
+      case 0b11100000 :
+        outstr->data[j] |= (Int)str.data[i] << 4;
+        outstr->data[j] |= (Int)str.data[i+1] << 2;
+        outstr->data[j] |= (Int)str.data[i+2] << 2;
+        i += 2;
+        break;
+      case 0b11110000 :
+        outstr->data[j] |= (Int)str.data[i] << 5;
+        outstr->data[j] |= (Int)str.data[i+1] << 2;
+        outstr->data[j] |= (Int)str.data[i+2] << 2;
+        outstr->data[j] |= (Int)str.data[i+3] << 2;
+        i += 3;
+        break;
+      default :
+        outstr->data[j] = (Int)str.data[i];
+        break;
+    }
+  }
+}
 
 #endif
