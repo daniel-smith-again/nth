d775626e933fd1a8de7374a965b032a3604230f9
Daniel Smith <daniel.smith.again@gmail.com>
2025-06-07 11:00:35 -0400

    ...

--- hyperspec/js/reference.js
+++ hyperspec/js/reference.js
@@ -36,14 +36,15 @@ var active = null;
 document['clipboard'] = null;
 area.onclick = (e)=> 
 {
-    if (area['clicklock']) {area['clicklock'] = false; return;}
-    if (e.target.tagName != 'NTH-AREA') return;
-    e.preventDefault()
-    var snippet = createSnippet()
-    area.appendChild(snippet)
-    var r = snippet.getBoundingClientRect();
-    snippet.style.top = String(Math.floor(e.clientY - (r.height / 2))) + 'px'
-    snippet.style.left = String(Math.floor(e.clientX - (r.width / 2))) + 'px'
+  console.log(e)
+  if (area['clicklock']) {area['clicklock'] = false; return;}
+  if (e.target.tagName != 'NTH-AREA') return;
+  e.preventDefault()
+  var snippet = createSnippet()
+  area.appendChild(snippet)
+  var r = snippet.getBoundingClientRect();
+  snippet.style.top = String(Math.floor(e.clientY - (r.height / 2))) + 'px'
+  snippet.style.left = String(Math.floor(e.clientX - (r.width / 2))) + 'px'
     
 }
 function createSnippet() 
@@ -74,36 +75,41 @@ function Close(e)
   area.removeChild(active)
 }
 
-function dragCode(e) 
+function dragCode(e)
 {
   e.preventDefault()
-  area['clicklock'] = true;
+  console.log(e)
+  area['clicklock'] = true
   var snippet = e.currentTarget
   for (var x = 0; x < area.childElementCount; x++)
     area.children[x].removeAttribute('active')
   snippet.setAttribute('active', 'true')
   active = snippet, active.children[0].focus()
-  const offsetY = snippet.offsetTop - (e.type == 'touchstart' ? e.touches[0].clientY : e.clientY)
-  const offsetX = snippet.offsetLeft - (e.type == 'touchstart' ? e.touches[0].clientX : e.clientX)
-  var move = function(e) 
-             {
-                e.preventDefault()
-                var x = (e.type == 'touchmove' ? e.touches[0].clientX : e.clientX) + offsetX
-                var y = (e.type == 'touchmove' ? e.touches[0].clientY : e.clientY) + offsetY
-                if ((x > 0) && (x + snippet.offsetWidth < window.innerWidth))
-                  snippet.style.left = String((e.type == 'touchmove' ? e.touches[0].clientX : e.clientX) + offsetX) + 'px'
-                if ((y > 0) && (y + snippet.offsetHeight < window.innerHeight))
-                  snippet.style.top = String((e.type == 'touchmove' ? e.touches[0].clientY : e.clientY) + offsetY) + 'px'
-             }
-  var stopmove = function(e)
-                 {
-                    e.preventDefault()
-                    document.onpointermove = null
-                    document.onpointerup = null
-                 }
+  const offsetY = snippet.offsetTop - e.clientY
+  const offsetX = snippet.offsetLeft - e.clientX
+  snippet.setPointerCapture(e.pointerId)
+  const move = function(e)
+  {
+    console.log(e)
+    e.preventDefault()
+    var x = e.clientX + offsetX
+    var y = e.clientY + offsetY
+    if ((x > 0) && (x + snippet.offsetWidth < window.innerWidth))
+      snippet.style.left = String(e.clientX + offsetX) + 'px'
+    if ((y > 0) && (y + snippet.offsetHeight < window.innerHeight))
+      snippet.style.top = String(e.clientY + offsetY) + 'px'
+  }
+  const stopmove = function(e)
+  {
+    e.preventDefault()
+    snippet.releasePointerCapture(e.pointerId)
+    document.onpointermove = null
+    document.onpointerup = null
+  }
   document.onpointermove = move
   document.onpointerup = stopmove
 }
+
 document.onkeydown = (e) => {
   if (document.activeElement.tagName == 'NTH-STRING') 
   {
@@ -157,7 +163,7 @@ function insertCharacter(c)
 {
   var caret = active.caret;
   if (caret.symbol == null || 
-      caret.symbol.tagName == 'BR' || 
+      caret.symbol.tagName == 'NTH-LINEBREAK' || 
       caret.symbol.tagName == 'NTH-EXPRESSION' || 
       caret.symbol.tagName == 'NTH-STRING' || 
       caret.symbol.getAttribute('tab') != null)
@@ -248,10 +254,15 @@ function insertBreak()
 {
   var caret = active.caret
   if (caret.tagName == 'NTH-CODE') return
+  
   //var b = document.createElement('span')
   //b.appendChild(document.createTextNode('LINEBREAK'))
   //b.appendChild(document.createElement('br'))
-  var b = document.createElement('br')
+  
+  //var b = document.createElement('br')
+  var b = document.createElement('nth-linebreak')
+  b.appendChild(document.createTextNode('↩'))
+  b.appendChild(document.createElement('br'));
   if (caret.childElementCount == 0) return
   else
   {
@@ -291,7 +302,7 @@ function backspace()
   var caret = active.caret
   if (caret.childElementCount > 0)
   {
-    if (caret.symbol.tagName !== 'NTH-EXPRESSION' && caret.symbol.textContent.length > 0 && caret.symbol.tagName != 'NTH-STRING')
+    if (caret.symbol.tagName !== 'NTH-EXPRESSION' && caret.symbol.tagName !== 'NTH-LINEBREAK' && caret.symbol.textContent.length > 0 && caret.symbol.tagName != 'NTH-STRING')
     {
       caret.symbol.textContent = caret.symbol.textContent.slice(0, -1)
       checkQuote(caret.symbol)
@@ -586,8 +597,8 @@ function structureToProgram(s)
           if (s.children[x].tagName != 'NTH-TAB' &&
               s.children[x].getAttribute('quote') == null &&
               s.children[x].getAttribute('tab') == null &&
-              s.children[x].tagName != 'BR' &&
-              s.children[x + 1].tagName != 'BR')
+              s.children[x].tagName != 'NTH-LINEBREAK' &&
+              s.children[x + 1].tagName != 'NTH-LINEBREAK')
           {
             p += ' '
           }
@@ -602,7 +613,7 @@ function structureToProgram(s)
     case 'NTH-STRING': 
       return '"' + s.innerText + '" '
     break
-    case 'BR': 
+    case 'NTH-LINEBREAK': 
       return '\n'
     break
   }
